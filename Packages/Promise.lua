52482a26e04e413a74c2d2b14bd1138ad88b201f
xSwezan <101433236+xSwezan@users.noreply.github.com>
2024-01-05 19:02:25 +0100

    v1.0.1

--- Packages/Promise.lua
+++ Packages/Promise.lua
@@ -1,1 +1,1 @@
-return require(script.Parent._Index["evaera_promise@4.0.0"]["promise"])
+return require(script.Parent._Index["lukadev-0_typed-promise@4.0.2"]["typed-promise"])
--- lib/init.lua
+++ lib/init.lua
@@ -1,25 +1,38 @@
 local RunService = game:GetService("RunService")
 local Promise = require(script.Parent.Promise)
-local Types = require(script.Types)
 
 local ConditionEnvironment = {}
 ConditionEnvironment.__index = ConditionEnvironment
+
+export type Environment = {
+	new: (Function: () -> nil) -> Promise.Promise;
+
+	Destroy: (self: Environment) -> nil;
+
+	AddCondition: (self: Environment, Name: string, Callback: () -> boolean, ConditionMet: () -> nil?) -> nil;
+	RemoveCondition: (self: Environment, string...) -> nil;
+}
 
 -->------------------<--
 --> Lifetime Methods <--
 -->------------------<--
 
-function ConditionEnvironment.new(Function: () -> nil): Types.ConditionEnvironment
+-- Returns a Promise, that when cancelled; destroys the Environment, thus making all the code stop running.
+function ConditionEnvironment.new(Function: () -> nil): Promise.Promise
 	local self = setmetatable({
 		Conditions = {};
 
 		__connections = {};
 	}, ConditionEnvironment)
 
-	self.PromiseThread = Promise.try(Function, self) :: Types.Promise
+	self.PromiseThread = Promise.try(Function, self) :: Promise.Promise
 
 	return Promise.new(function(resolve, reject, onCancel)
 		self.PromiseThread:andThen(resolve)
+
+		onCancel(function()
+			self:Destroy()
+		end)
 
 		table.insert(self.__connections, RunService.Heartbeat:Connect(function()
 			for Name: string, ConditionInfo: {Callback: () -> boolean?, ConditionMet: () -> nil?} in self.Conditions or {} do
@@ -40,6 +53,7 @@ function ConditionEnvironment.new(Function: () -> nil): Types.ConditionEnvironme
 	end)
 end
 
+-- Stops the current code and destroys the Environment
 function ConditionEnvironment:Destroy()
 	for _, Connection: RBXScriptConnection in self.__connections do
 		if not (Connection.Connected) then continue end
@@ -58,6 +72,16 @@ end
 --> Methods <--
 -->---------<--
 
+--[=[
+	Add a condition that when met; destroys the environment, thus making all the code stop running.
+	```lua
+	Environment:AddCondition("IsEnoughPlayers", function()
+		return (#Players:GetPlayers() >= 2)
+	end, function()
+		print("Someone left!")
+	end)
+	```
+]=]
 function ConditionEnvironment:AddCondition(Name: string, Callback: () -> boolean, ConditionMet: () -> nil?)
 	self.Conditions[Name] = {
 		Callback = Callback;
@@ -65,6 +89,12 @@ function ConditionEnvironment:AddCondition(Name: string, Callback: () -> boolean
 	}
 end
 
+--[=[
+	Removes a condition from the environment. Multiple conditions can be removed at the same time by simply providing more condition names.
+	```lua
+	Environment:RemoveCondition("IsEnoughPlayers", "IsThereAWinner")
+	```
+]=]
 function ConditionEnvironment:RemoveCondition(...: string)
 	for _, Name: string in {...} do
 		if (type(Name) ~= "string") then continue end
